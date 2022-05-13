a25c64f2dc9d5196dc104ea3c6518b40df3fc556
ezelu <ezelujhay@gmail.com>
2022-05-13 15:40:45 -0700

    Updated modal

--- src/components/ContentModal/ContentModal.js
+++ src/components/ContentModal/ContentModal.js
@@ -22,7 +22,7 @@
 //   const handleOpen = () => setOpen(true);
 //   const handleClose = () => setOpen(false);
 
-//   const fetchData = async () => {
+//   const fetchInfo = async () => {
 //     const {data} = await axios.get(
 //       `https://api.themoviedb.org/3/${media_type}/${id}?api_key=e002f08f46e0049891b3812857957fab&language=en-US`
 //     )
@@ -90,13 +90,19 @@
 
 
 
-import * as React from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import './ContentModal.css';
 import axios from 'axios';
+
+
+
+
+
+
 
 const style = {
   position: 'absolute',
@@ -114,12 +120,48 @@ export default function ContentModal({children, media_type, id}) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [contents, set_contents] = React.useState();
+  const [loading, set_loading] = React.useState(false);
+  const [loading2, set_loading2] = React.useState(false);
+  const [trailer, set_trailer] = React.useState('')
 
-  const fetchData = async () => {
+
+
+
+  const fetchInfo = async () => {
+    set_loading(true)
     const {data} = await axios.get(
-      `https://api.themoviedb.org/3/${media_type}/${id}?api_key=e002f08f46e0049891b3812857957fab&language=en-US`
-    )
+      `https://api.themoviedb.org/3/${media_type}/${id}?api_key=e002f08f46e0049891b3812857957fab&language=en-US`);
+
+    set_loading(false);
+    set_contents(data);
+    console.log(data)
   }
+
+
+  const fetchTrailer = async () => {
+    set_loading2(true)
+    const {data} = await axios.get(
+      `https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=e002f08f46e0049891b3812857957fab&language=en-US`);
+
+
+    console.log(data.results[0]?.key)
+    set_loading2(false);
+    set_contents(data.results[0]?.key);
+  }
+
+  React.useEffect(() => {
+    fetchInfo();
+    fetchTrailer()
+  }, [])
+
+  
+
+
+
+
+
+
 
 
 
@@ -140,12 +182,11 @@ export default function ContentModal({children, media_type, id}) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          
+
+
+
+
         </Box>
       </Modal>
     </div>
