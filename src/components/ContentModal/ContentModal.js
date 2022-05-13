c0ad28f3a1dddb397da1484c4ab24168d15fd2ca
ezelu <ezelujhay@gmail.com>
2022-05-13 15:27:42 -0700

    Updated Modal

--- src/components/ContentModal/ContentModal.js
+++ src/components/ContentModal/ContentModal.js
@@ -1,30 +1,138 @@
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
+// import Modal from '@mui/material/Modal';
+// import './ContentModal.css';
+
+// const style = {
+//   position: 'absolute',
+//   top: '50%',
+//   left: '50%',
+//   transform: 'translate(-50%, -50%)',
+//   width: "90%",
+//   bgcolor: '#39445a',
+//   // border: '2px solid #000',
+//   boxShadow: 24,
+//   p: 4,
+// };
+
+// export default function ContentModal({children, media_type, id}) {
+//   const [open, setOpen] = React.useState(false);
+//   const handleOpen = () => setOpen(true);
+//   const handleClose = () => setOpen(false);
+
+//   const fetchData = async () => {
+//     const {data} = await axios.get(
+//       `https://api.themoviedb.org/3/${media_type}/${id}?api_key=e002f08f46e0049891b3812857957fab&language=en-US`
+//     )
+//   }
+
+
+
+
+
+
+
+
+
+
+//   return (
+//     <div>
+//       <Button onClick={handleOpen} className='media'> {children} </Button>
+//       <Modal
+//         open={open}
+//         onClose={handleClose}
+//         aria-labelledby="modal-modal-title"
+//         aria-describedby="modal-modal-description"
+//       >
+//         <Box sx={style}>
+//           <Typography id="modal-modal-title" variant="h6" component="h2">
+//             Text in a modal
+//           </Typography>
+//           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+//             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+//           </Typography>
+//         </Box>
+//       </Modal>
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import './ContentModal.css';
+import axios from 'axios';
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  width: "90%",
+  bgcolor: '#39445a',
+  // border: '2px solid #000',
   boxShadow: 24,
   p: 4,
 };
 
-export default function BasicModal() {
+export default function ContentModal({children, media_type, id}) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  const fetchData = async () => {
+    const {data} = await axios.get(
+      `https://api.themoviedb.org/3/${media_type}/${id}?api_key=e002f08f46e0049891b3812857957fab&language=en-US`
+    )
+  }
+
+
+
+
+
+
+
+
+
+
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
+      <Button onClick={handleOpen} className='media'> {children} </Button>
       <Modal
         open={open}
         onClose={handleClose}
--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,7 +1,73 @@
+// import React from 'react';
+// import './SingleContent.css'
+// import { img_300, unavailable } from '../../config/config'
+// import { Badge } from '@material-ui/core'
+
+
+// export default function SingleContent(props) {
+
+//   const {id, poster, title, date, media_type, vote_average} = props;
+
+
+
+
+//   return (
+//     <div className='media'>
+//         <Badge badgeContent={vote_average} 
+//         color={vote_average > 6 ? 'primary' : 'secondary' }/>
+        
+//         <img src={ poster ? `${img_300}${poster}` : unavailable } alt={title} className='poster'/>
+//         <b className='title'> {title} </b>
+//         <span className='subtitle'>
+//         <span className='type'> {media_type === 'movie' ? 'Movie' : 'TV Series'} </span>
+//         <span className='date'> {date} </span>
+//       </span>
+
+//     </div>
+//   )
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React from 'react';
 import './SingleContent.css'
 import { img_300, unavailable } from '../../config/config'
 import { Badge } from '@material-ui/core'
+import ContentModal from '../ContentModal/ContentModal';
 
 
 export default function SingleContent(props) {
@@ -12,7 +78,7 @@ export default function SingleContent(props) {
 
 
   return (
-    <div className='media'>
+    <ContentModal media_type={media_type} id={id}>
         <Badge badgeContent={vote_average} 
         color={vote_average > 6 ? 'primary' : 'secondary' }/>
         
@@ -23,6 +89,6 @@ export default function SingleContent(props) {
         <span className='date'> {date} </span>
       </span>
 
-    </div>
+    </ContentModal>
   )
 }
